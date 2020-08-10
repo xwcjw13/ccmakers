de35d3437191bace1155f0b1c6083609c74547c0
xwcjw13 <69455714+xwcjw13@users.noreply.github.com>
2020-08-10 17:49:30 +0900

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,352 @@
+W = [];
+
+function Illust(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].tool == "Illustrator"){
+			document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+		}
+	}	
+}
+
+function Photo(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].tool == "Photoshop"){
+			document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+		}
+	}
+}
+
+function After(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].tool == "After Effect"){
+			document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+		}
+	}
+}
+
+function GJB(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "16 고정빈"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function KCY(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "16 구채연"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function RSR(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "16 류솔아"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function USY(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "16 엄서영"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function YGO(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "16 염가온"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function KDY(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "17 김도연"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function KHK(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "17 김현경"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function YHY(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "17 윤희연"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function BJH(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "17 박지혜"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}
+		}
+	}	
+}
+
+function CJW(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "17 최지원"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}
+		}
+	}	
+}
+
+function KMJ(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "18 김민정"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function KSB(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "18 김수빈"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function SHE(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "18 송하은"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function SHI(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "18 신혜인"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function SHL(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "19 신혜림"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+function JSM(){
+	document.getElementById('table').innerHTML = "";
+	for(var i = 0; i<W.length; i++){
+		if(W[i].designer == "19 정승민"){
+			if(W[i].tool !== "After Effect"){
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<img src='"+W[i].work+"'>");
+
+			}
+			else{
+				document.getElementById('table').insertAdjacentHTML("beforeend", "<video controls><source src='"+W[i].work+"' type='video/mp4'></video>");
+
+			}			
+		}
+	}	
+}
+
+
+var W=[{work: "1643004 고정빈 포스터 복사본.png", tool: "Photoshop", designer: "16 고정빈"},
+		{work:"1644006_류솔아_1_편집 복사본.png", tool: "Photoshop", designer: "16 류솔아"},
+		{work: "1744004김도연_2 복사본.jpg", tool: "Photoshop", designer: "17 김도연"},
+		{work: "염가온_2 복사본.png", tool: "Photoshop", designer: "16 염가온"},
+		{work: "1644006_류솔아_2_편집 복사본.png", tool: "Photoshop", designer: "16 류솔아"},
+		{work: "1743036 윤희연1 복사본.png", tool: "Photoshop", designer: "17 윤희연"},
+		{work: "염가온_1 복사본.png", tool: "Photoshop", designer: "16 염가온"},
+		{work: "1743036 윤희연2 복사본.png", tool: "Photoshop", designer: "17 윤희연"},
+		{work: "1744004김도연_poster_2 복사본.png", tool: "Illustrator", designer: "17 김도연"},
+		{work: "1743036 윤희연.png", tool: "Illustrator", designer: "17 윤희연"},
+		{work: "1743052 최지원_자기소개_최종.png", tool: "Illustrator", designer: "17 최지원"},
+		{work: "1644006_류솔아 복사본.png", tool: "Illustrator", designer: "16 류솔아"},
+		{work: "1643006구채연_최종.png", tool: "Illustrator", designer: "16 구채연"}, 
+		{work: "1744027_박지혜_포스터2.jpg", tool: "Photoshop", designer: "17 박지혜"},
+		{work: "1743052 최지원 복사본.png", tool: "Illustrator", designer: "17 최지원"}, 
+		{work: "1744007 김현경 씨씨메.png", tool: "Illustrator", designer: "17 김현경"},
+		{work: "1744007 김현경_1편집 복사본.png", tool: "Photoshop", designer: "17 김현경"},
+		{work: "1744007 김현경_2편집 복사본.png", tool: "Photoshop", designer: "17 김현경"},
+		{work: "1744027_박지혜_포스터1.jpg", tool: "Photoshop", designer: "17 박지혜"},
+		{work: "1844004 김민정 CC메.png", tool: "Illustrator", designer: "18 김민정"},
+		{work: "1844004 김민정 1 복사본.jpg", tool: "Photoshop", designer: "18 김민정"},
+		{work: "1844004 김민정2 복사본.jpg", tool: "Photoshop", designer: "18 김민정"},
+		{work: "1844006 김수빈 포스터.jpg", tool: "Illustrator", designer: "18 김수빈"},		
+		{work: "1844006 김수빈 캡틴마블1.png", tool: "Photoshop", designer: "18 김수빈"},		
+		{work: "1844006 김수빈 캡틴마블2.png", tool: "Photoshop", designer: "18 김수빈"},		
+		{work: "2차과제_1844015_송하은.png", tool: "Illustrator", designer: "18 송하은"},		
+		{work: "1844015_송하은_1.png", tool: "Photoshop", designer: "18 송하은"},		
+		{work: "1844015_송하은_2.png", tool: "Photoshop", designer: "18 송하은"},		
+		{work: "1844017신혜인_그부다1.jpg", tool: "Photoshop", designer: "18 신혜인"},		
+		{work: "1844017_신혜인_그부다2.jpg", tool: "Photoshop", designer: "18 신혜인"},		
+		{work: "1844017 신혜인.png", tool: "Illustrator", designer: "18 신혜인"},		
+		{work: "1944015 신혜림 2-01.png", tool: "Illustrator", designer: "19 신혜림"},		
+		{work: "1944015 신혜림 포스터 최종.png", tool: "Photoshop", designer: "19 신혜림"},		
+		{work: "1944015 신혜림 포스터2 최종.png", tool: "Photoshop", designer: "19 신혜림"},		
+		{work: "1944027 정승민1캡쳐.png", tool: "Photoshop", designer: "19 정승민"},		
+		{work: "1944027 -정승민.png", tool: "Photoshop", designer: "19 정승민"},		
+
+		{work: "1643004 walking(변환됨).mov", tool: "After Effect", designer: "16 고정빈"},
+		{work: "1743036 윤희연(변환됨).mov", tool: "After Effect", designer: "17 윤희연"},
+		{work: "1743036 윤희연_이름.mp4", tool: "After Effect", designer: "17 윤희연"},
+		{work: "1743052 최지원 걸어가기.mov", tool: "After Effect", designer: "17 최지원"},
+		{work: "1743052 최지원.mp4", tool: "After Effect", designer: "17 최지원"},
+		{work: "1743052 최지원2.mp4", tool: "After Effect", designer: "17 최지원"},
+		{work: "1644006_류솔아.mp4", tool: "After Effect", designer: "16 류솔아"},		
+		{work: "1644013_엄서영_로고_1.mp4", tool: "After Effect", designer: "16 엄서영"},		
+		{work: "1744004 김도연.mp4", tool: "After Effect", designer: "17 김도연"},		
+		{work: "1744007_김현경.mov", tool: "After Effect", designer: "17 김현경"},
+		{work: "김현경.mp4", tool: "After Effect", designer: "17 김현경"},
+		{work: "구채연구채연(변환됨).mov", tool: "After Effect", designer: "16 구채연"},		
+		{work: "애니메이션_1644014염가온.mp4", tool: "After Effect", designer: "16 염가온"},						
+		{work: "1744027박지혜_캐릭터애니메이션과제(변환됨).mov", tool: "After Effect", designer: "17 박지혜"},		
+		{work: "1744027박지혜_이름애니메이션(변환됨).mov", tool: "After Effect", designer: "17 박지혜"},		
+		{work: "1844004 김민정1(변환됨).mov", tool: "After Effect", designer: "18 김민정"},		
+		{work: "김민정(변환됨).mov", tool: "After Effect", designer: "18 김민정"},		
+		{work: "1844006_김수빈_레오(변환됨).mov", tool: "After Effect", designer: "18 김수빈"},		
+		{work: "1844006_김수빈_로고(변환됨).mov", tool: "After Effect", designer: "18 김수빈"},		
+		{work: "1844015_송하은걸어가기(변환됨).mov", tool: "After Effect", designer: "18 송하은"},		
+		{work: "1844015 송하은로고(변환됨).mov", tool: "After Effect", designer: "18 송하은"},		
+		{work: "1844017신혜인(변환됨).mov", tool: "After Effect", designer: "18 신혜인"},		
+		{work: "1844017신헤인로고(변환됨).mov", tool: "After Effect", designer: "18 신혜인"},		
+		{work: "1944015 신혜림(변환됨).mov", tool: "After Effect", designer: "19 신혜림"},		
+		{work: "1944027 - 정승민(변환됨).mov", tool: "After Effect", designer: "19 정승민"},		
+
+
+		];
+
